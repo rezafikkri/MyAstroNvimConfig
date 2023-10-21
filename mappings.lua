066f02bc81aa9f5d5ff9bc5f89a8e7d917cb7dfc
rezafikkri <fikkri.reza@gmail.com>
2023-10-21 14:34:55 +0700

    feat: add cutom mapping for toggle terminal

--- mappings.lua
+++ mappings.lua
@@ -32,9 +32,11 @@ return {
     ["<leader>b"] = { name = "Buffers" },
     -- quick save
     -- ["<C-s>"] = { ":w!<cr>", desc = "Save File" },  -- change description but the same command
+    ["<C-\\>"] = { "<cmd>ToggleTerm<cr>", desc = "Toggle terminal" }
   },
   t = {
     -- setting a mapping to false will disable it
     -- ["<esc>"] = false,
+    ["<C-\\>"] = { "<cmd>ToggleTerm<cr>", desc = "Toggle terminal" }
   },
 }
